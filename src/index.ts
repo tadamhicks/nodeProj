b07a08b9f89d8a8dc0abda2ae05c84e570764b87
Adam Hicks <adam.hicks@trace3.com>
2023-12-28 16:22:42 -0500

    Add Docker and escode config

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log('Hello World!');
+import express from 'express';
+import morgan from 'morgan';
+
+const app = express();
+app.use(morgan('dev'));
+
+app.get('/', (req, res) => {
+  res.json({ hello: 'world' });
+});
+
+const port = Number(process.env.PORT || 8080);
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Listening at http://localhost:${port}`);
+});
