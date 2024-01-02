fa318f777ae1da486acdd349683a34a0f6337e0c
Adam Hicks <adam.hicks@trace3.com>
2024-01-02 10:29:09 -0500

    Additional Routes

--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,25 @@ import { db } from './modules/db';
 const app = express();
 app.use(morgan('dev'));
 
-app.get('/', async(req, res) => {
+app.get('/', (req, res) => {
+  res.json({ message: 'Hello World!' });
+});
+app.get('/posts', async (req, res) => {
   const posts = await db.post.findMany({
     where: { publishedAt: { not: null } },
   });
   res.json(posts);
+});
+
+app.get('/posts/:id', async (req, res) => {
+  const { id } = req.params;
+  const post = await db.post.findUnique({
+    where: {
+      id: String(id),
+      publishedAt: { not: null },
+    },
+  });
+  res.json(post);
 });
 
 const port = Number(process.env.PORT || 8080);
